c9a00b94bec5aba803ffeece9a5f4d0b47955954
tiennv0608 <tiennv0608@gmail.com>
2021-05-25 08:26:19 +0700

    b5

---
+++ 05_B5_CaidatInsertionSort/src/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort {
+
+    static void insertionSort(int[] array) {
+        int position, value;
+        for (int i = 1; i < array.length; i++) {
+            value = array[i];
+            position = i;
+
+            while (position > 0 && value < array[position - 1]) {
+                array[position] = array[position - 1];
+                position--;
+            }
+            array[position] = value;
+        }
+    }
+
+    static void display(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] list = {12, 6, 3, 19, 5, 2, 1, 14};
+        System.out.println("Truoc khi sap xep");
+        display(list);
+        insertionSort(list);
+        System.out.println("Sau khi sap xep");
+        display(list);
+    }
+}
